3fce5c9762ba20a26ea028c0ac6eb6a9159b4c4d
MionaTanaka <104816812+Miomon3@users.noreply.github.com>
2022-07-23 04:15:44 +0900

    課題提出

--- src/practice/Chapter10.java
+++ src/practice/Chapter10.java
@@ -6,14 +6,32 @@ import java.util.List;
 
 public class Chapter10 {
 
-	public static void main(String[] args) {
-		List<Task> list = new ArrayList<>();
-        list.add(new Task(LocalDate.of(2021, 10, 21), "牛乳を買う", true));
-        list.add(new Task(LocalDate.of(2021, 9, 15), "○○社面接", false));
-        list.add(new Task(LocalDate.of(2021, 12, 4), "手帳を買う", true));
-        list.add(new Task(LocalDate.of(2021, 8, 10), "散髪に行く", false));
-        list.add(new Task(LocalDate.of(2021, 11, 9), "スクールの課題を解く", false));
+	public static void main(String[] args) throws Exception {
 
+		List<Task> list = new ArrayList<>();
+		list.add(new Task(LocalDate.of(2021, 10, 21), "牛乳を買う", true));
+		list.add(new Task(LocalDate.of(2021, 9, 15), "○○社面接", false));
+		list.add(new Task(LocalDate.of(2021, 12, 4), "手帳を買う", true));
+		list.add(new Task(LocalDate.of(2021, 8, 10), "散髪に行く", false));
+		list.add(new Task(LocalDate.of(2021, 11, 9), "スクールの課題を解く", false));
+
+		long count;
+		count = list.stream().filter(t -> t.isDone() == false).count();
+		System.out.println("未完了のタスクの個数は" + count);
+		
+		System.out.println();
+		
+		try {
+			list.stream()
+		     .filter(n -> n.isDone() == false)
+		     .sorted()
+		     .forEach(System.out::println);
+		} catch (Exception e) {
+			System.out.println("Exception例外が発生");
+		} finally {
+			System.out.println();
+		}
+		
 	}
 
 }
